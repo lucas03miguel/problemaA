74fd5591d50a3a7ddca569c849bfa7bd141374f7
Lucas <lucas.m.loberto@gmail.com>
2024-03-10 00:50:43 +0000

    45 pontos

--- src/aztec.py
+++ src/aztec.py
@@ -1,3 +1,6 @@
+from collections import deque
+from copy import deepcopy
+
 def rodar_esquerda(grid):
     new_grid = [[0, 0], [0, 0]]
     for i in range(len(grid)):
@@ -13,55 +16,68 @@ def rodar_direita(grid):
     return new_grid
 
 def rodar(grid, row, col, lado):
-    new_grid = [grid[row][col:col + 2], grid[row + 1][col:col + 2]]
-    print(new_grid)
+    grid_copia = deepcopy(grid)
+    new_grid = [grid_copia[row][col:col + 2], grid_copia[row + 1][col:col + 2]]
+    #print(new_grid)
 
-    
     if lado == "esquerda":
         new_grid = rodar_esquerda(new_grid)
     else:
         new_grid = rodar_direita(new_grid)
-    print(new_grid)
+    #print(new_grid)
 
-    grid[row][col:col + 2] = new_grid[0]
-    grid[row + 1][col:col + 2] = new_grid[1]
-    return grid
+    grid_copia[row][col:col + 2] = new_grid[0]
+    grid_copia[row + 1][col:col + 2] = new_grid[1]
+
+    # Retorna a cópia atualizada
+    return grid_copia
 
 
 
 def aztec(grid, moves):
+    objetivo = [(len(grid[0])) * [(i + 1)] for i in range(len(grid))]
+    
+    fronteira = deque()
+    fronteira.append((grid, 0))
+    visitados = []
+    visitados.append(grid)
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            pass
+    while fronteira:
+        estadoAtual, movimentos = fronteira.popleft()
+        
+        if estadoAtual == objetivo:
+            return movimentos
+        
+        if movimentos >= moves:
+            continue 
+        
+        for i in range(len(grid) - 1):
+            for j in range(len(grid[i]) - 1):
+                for rotacao in ["esquerda", "direita"]:
+                    novoEstado = rodar(estadoAtual, i, j, rotacao)
 
-    start_row = 0
-    start_col = 0
-    aux = rodar(grid, start_row, start_col, "direita")
-    print("rodou:\n", aux) 
+                    if novoEstado == objetivo:
+                        return movimentos + 1
+                    
+                    if novoEstado not in visitados:
+                        visitados.append(novoEstado)
+                        fronteira.append((novoEstado, movimentos + 1))
 
-
-    return 0
+    return "the treasure is lost!"
 
 
 def main():
     T = int(input())
-    for i in range(T):
+    for _ in range(T):
         rows, columns, moves = map(int, input().split())
-        G = [[0 for i in range(columns)] for j in range(rows)]
+        G = []
 
         for j in range(rows):
-            G[j][:] = map(int, input().split())
+            row = map(int, input().split())
+            G.append(list(row))
         
-        print(G)
         min = aztec(G, moves)	
-
-        if min == -1:
-            print("the treasure is lost!")
-        else:
-            print(min)
-
-
+        print(min)
 
 
 if __name__ == "__main__":
